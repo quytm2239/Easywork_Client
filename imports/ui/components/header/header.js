6500dcd5d22da2e3fc9dbc50087d576645666626
long.dang.thanh <long.dang.thanh@sofiamedix.vn>
2017-10-19 13:38:38 +0700

    update header

--- imports/ui/components/header/header.js
+++ imports/ui/components/header/header.js
@@ -3,7 +3,7 @@ import './header.html';
 
 Template.header.events({
     'click #search'(event, instance) {
-        $('#search').stop().animate({width:230},500);
+        $('#search').stop().animate({width:200},500);
     }, 
     'focusout #search'(event, instance) {
         $('#search').stop().animate({width:100},500);
