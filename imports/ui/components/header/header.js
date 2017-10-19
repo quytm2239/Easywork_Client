c30cf2476877ce387677a0c8a28b59d45d3460ca
long.dang.thanh <long.dang.thanh@sofiamedix.vn>
2017-10-19 11:44:57 +0700

    update header

--- imports/ui/components/header/header.js
+++ imports/ui/components/header/header.js
@@ -1,2 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 import './header.html';
+
+Template.header.events({
+    'click #search'(event, instance) {
+        $('#search').stop().animate({width:230},500);
+    }, 
+    'focusout #search'(event, instance) {
+        $('#search').stop().animate({width:100},500);
+    }, 
+});
