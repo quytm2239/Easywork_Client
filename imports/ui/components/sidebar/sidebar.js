2cfac3f19f92200094fdb4d9e2c6f429c0c64db1
quytm2239 <quytm2239@gmail.com>
2017-10-20 23:29:37 +0700

    Update sidebar UX

--- imports/ui/components/sidebar/sidebar.js
+++ imports/ui/components/sidebar/sidebar.js
@@ -10,24 +10,19 @@ const MAX_PROJECT_INFO_HEIGHT = 250;
 
 Template.sidebar.onRendered(function () {
     // counter starts at 0
-    console.log(this);
 });
 
 Template.sidebar.events({
 
     'click #iconCollapseProjectInfo'(event, instance) {
-        if (instance.isFullProjectContent == false) {
-            if (instance.isFullSizeBar == false) {
-                return;
-            }
-            instance.isFullProjectContent = true;
-            clickProjectInfoIcon(instance.isFullProjectContent);
-        } else {
-            instance.isFullProjectContent = false;
-            clickProjectInfoIcon(instance.isFullProjectContent);
+        if (instance.isFullProjectContent == false
+             && instance.isFullSizeBar == false) {
+            return;
         }
+        instance.isFullProjectContent = !instance.isFullProjectContent;
+        clickProjectInfoIcon(instance.isFullProjectContent);
     },
-    'click #iconCollapseSideBar'(event, instance) {
+    'click #buttonCollapseSideBar'(event, instance) {
         if (instance.isFullSizeBar) {
             instance.isFullSizeBar = false;
             instance.isFullProjectContent = false;
@@ -41,6 +36,6 @@ Template.sidebar.events({
 function clickProjectInfoIcon(isFullProjectContent) {
     var projectInfoHeight = isFullProjectContent ? MAX_PROJECT_INFO_HEIGHT : MIN_PROJECT_INFO_HEIGHT;
     $('.side-bar-project-info').height(projectInfoHeight);
-    $('.side-bar-project-info-content').css('hidden',isFullProjectContent ? 'true' : 'false');
+    $('.side-bar-project-info-content').css('display',isFullProjectContent ? 'block' : 'none');
     $('#iconCollapseProjectInfo').text(isFullProjectContent ? 'keyboard_arrow_up' : 'keyboard_arrow_down');
 }
